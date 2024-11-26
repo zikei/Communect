5e7ea565d406663d4f3b27dc6fa829bf60aa9896
Shun-art <dqfrom@yahoo.co.jp>
2024-11-26 14:51:44 +0900

    TopMock画面の追加及び多々修正

--- web/frontend/communect/src/main.jsx
+++ web/frontend/communect/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import Top from './Top.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Top />
   </StrictMode>,
 )
--- web/frontend/communect/vite.config.js
+++ web/frontend/communect/vite.config.js
@@ -7,5 +7,8 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000
+  },
+  alias: {
+    'granim': 'granim/dist/granim.min.js'
   }
 })
