b3bbefc426a2dd361e72d0a94ed9d980dfd5a306
mizumotokazuki <mikazuki2763@gmail.com>
2024-12-16 11:17:16 +0900

    Add Setting

--- mobile/Communect/App.js
+++ mobile/Communect/App.js
@@ -3,6 +3,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './Login';
 import Group from './Group';
+import MockGroup from './MockGroup';
+import Setting from './Setting';
+import Register from './Register';
+import AccountEdit from './AccountEdit';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +16,10 @@ const App = () => {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         <Stack.Screen name="Group" component={Group} options={{ title: 'グループ' }} />
+        {/*<Stack.Screen name="Group" component={MockGroup} options={{ title: 'グループ' }} />*/}
+        <Stack.Screen name="Setting" component={Setting} options={{ title: '設定' }} />
+        <Stack.Screen name="Register" component={Register} options={{ title: 'アカウント登録' }} />
+        <Stack.Screen name="AccountEdit" component={AccountEdit} options={{ title: 'アカウント設定' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
